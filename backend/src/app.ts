4a67be21d2da2fe560882253c893bf1118e726a6
Elwan <144356501+ElwanBeauchot@users.noreply.github.com>
2026-02-09 16:25:05 +0100

    [BACK] repositories & dto & controllers & routes & services & mocks

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import userRoutes from "./routes/user.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
     res.send("API running 🚀");
